f56e776e8815b15ea1a382edf11f907557d08311
Brandon Burning <brandonb@ecreations.net>
2017-08-18 16:30:40 -0700

    Lots of stuff

--- app/sockets.js
+++ app/sockets.js
@@ -2,24 +2,134 @@
 
 module.exports = function (io, ADMIN_PASSWORD) {
   var messages = [];
+  var playedVideos = [];
+  var playlist = [];
   var users = [];
+  var usersLower = [];
+  var userVideos = {};
+  var activeVideo;
+
+  function clearData () {
+    activeVideo = null;
+    messages = [];
+    playedVideos = [];
+    playlist = [];
+    users = [];
+    usersLower = [];
+    userVideos = {};
+  }
+
+  function getUsernameIndex (username) {
+    return usersLower.indexOf(username.toLowerCase());
+  }
+
+  function getVideoIndex (video) {
+    return playlist.map(function (v) { return v.id.videoId; }).indexOf(video.id.videoId);
+  }
 
   io.sockets.on('connection', function (socket) {
     var username;
 
-    // send initial data to new user
-    socket.emit('socket connected', {});
+    // fresh data for first connection
+    if (!users.length) {
+      clearData();
+    }
 
-    socket.on('refresh socket response', function () {
+    // only allow 15 users
+    if (users.length >= 15) {
+      return;
+    }
+
+    // send initial data to new user
+    socket.emit('socket connected', {
+      messages: messages,
+      playedVideos: playedVideos,
+      playlist: playlist,
+      users: users
+    });
+
+    socket.on('login request', function (requestedUsername) {
+
+      // not a valid username
+      if (!requestedUsername) {
+        socket.emit('login error');
+        return;
+      }
+
+      // not a unique username
+      if (getUsernameIndex(requestedUsername) > -1) {
+        socket.emit('login error');
+        return;
+      }
+
+      username = requestedUsername;
+      users.push(requestedUsername);
+      usersLower.push(requestedUsername.toLowerCase());
+
+      io.sockets.emit('user connected', username);
+      socket.emit('login success');
+    });
+
+    socket.on('current video request', function () {
+      if (!activeVideo) {
+        return;
+      }
+
+      socket.emit('current video response', {
+        startSeconds: activeVideo.startSeconds,
+        video: activeVideo
+      });
+    });
+
+    socket.on('update video response', function (data) {
+      activeVideo = data;
+      userVideos[username] = data;
+    });
+
+    socket.on('video added', function (video) {
+      if (getVideoIndex(video) > -1) {
+        return;
+      }
+
+      playlist.push(video);
+
+      io.sockets.emit('add video to playlist', video);
+    });
+
+    socket.on('video deleted', function (video) {
+      var index = getVideoIndex(video);
+
+      if (index > -1) {
+        playlist.splice(index, 1);
+      }
+
+      io.sockets.emit('delete video from playlist', video);
+    });
+
+    socket.on('socket refresh response', function () {
       // do nothing, keep socket alive
     });
 
     socket.on('disconnect', function () {
+
+      if (!username) {
+        return;
+      }
+
+      var index = getUsernameIndex(username);
+      users.splice(index, 1);
+      usersLower.splice(index, 1);
+
+      if (!users.length) {
+        clearData();
+      }
+
+      io.sockets.emit('user disconnected', username);
     });
   });
 
   // ping sockets to keep connections open
   setInterval(function () {
-    io.sockets.emit('refresh socket request');
+    io.sockets.emit('socket refresh request');
   }, 15000);
 };
---
+++ test/main/main.controller.test.js
@@ -0,0 +1,339 @@
+describe('MainCtrl', function () {
+
+  beforeEach(module('somethingfm'));
+
+  // placeholders
+  var $controller;
+  var $httpBackend;
+  var $scope;
+
+  // mocks
+  var SocketService;
+  var VideoService;
+
+  var searchResults = {
+    data: {
+      items: [
+        {
+          title: 'Title'
+        }
+      ]
+    }
+  };
+
+  var sandbox;
+  var socketEvents = {};
+
+  beforeEach(inject(function(_$controller_, _$httpBackend_, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    $scope = $rootScope.$new();
+
+    sandbox = sinon.sandbox.create();
+
+    SocketService = {
+      on: function (evt, callback) { socketEvents[evt] = callback },
+      emit: sandbox.stub()
+    };
+
+    VideoService = {
+      getYoutube: sandbox.stub(),
+      launchPlayer: sandbox.stub(),
+      search: sandbox.stub()
+    };
+
+    $controller = _$controller_('MainCtrl', {
+      $scope: $scope,
+      SocketService: SocketService,
+      VideoService: VideoService
+    });
+
+    $scope.$apply();
+  }));
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it('should exist', function () {
+    expect($controller).toBeDefined();
+  });
+
+  describe('.saveUser', function () {
+
+    it('ignores empty or non-unique usernames', function () {
+      var username = 'username';
+
+      spyOn(SocketService, 'emit').and.callThrough();
+
+      $controller.users = [username];
+      $controller.usersLower = [username];
+
+      // no username
+      $controller.saveUser();
+
+      // non-unique username
+      $controller.username = username;
+      $controller.saveUser();
+
+      expect(SocketService.emit).not.toHaveBeenCalled();
+    });
+
+    it('calls SocketService.emit with valid username', function () {
+      var username = 'username';
+
+      spyOn(SocketService, 'emit').and.callThrough();
+
+      $controller.username = username;
+      $controller.saveUser();
+
+      expect(SocketService.emit).toHaveBeenCalledWith('login request', username);
+    });
+  });
+
+  describe('.loadVideo', function () {
+
+    it('calls VideoService.launchPlayer', function () {
+      var currentVideo = { property: true };
+
+      spyOn(VideoService, 'launchPlayer').and.callThrough();
+
+      $controller.currentVideo = currentVideo;
+      $controller.loadVideo();
+
+      expect(VideoService.launchPlayer).toHaveBeenCalledWith(currentVideo);
+    });
+  });
+
+  describe('.search', function () {
+
+    it('calls VideoService.search and sets settings', inject(function ($q) {
+      var query = 'query';
+
+      VideoService.search.returns($q.when(searchResults));
+
+      $controller.query = query;
+      $controller.search();
+
+      $scope.$apply();
+
+      expect(VideoService.search.calledWith(query)).toBe(true);
+      expect($controller.searchQuery).toEqual(query);
+      expect($controller.results).toEqual(searchResults.data.items);
+    }));
+  });
+
+  describe('.add', function () {
+
+    it('returns if video exists in playlist', function () {
+      var video = {
+        id: {
+          videoId: '1'
+        }
+      };
+
+      spyOn(SocketService, 'emit').and.callThrough();
+
+      $controller.playlist = [video];
+      $controller.add(0, video);
+
+      expect(SocketService.emit).not.toHaveBeenCalled();
+    });
+
+    it('removes video from search results, adds username, and calls SocketService.emit', function () {
+      var username = 'username';
+      var video = {
+        id: {
+          videoId: '1'
+        }
+      };
+
+      spyOn(SocketService, 'emit').and.callThrough();
+
+      $controller.results = [video];
+      $controller.username = username;
+      $controller.add(0, video);
+
+      expect($controller.results.length).toEqual(0);
+      expect(SocketService.emit).toHaveBeenCalledWith('video added', $.extend({}, video, { username: username }));
+    });
+  });
+
+  describe('.clearSearch', function () {
+
+    it('clears search results', function () {
+      $controller.query = 'query';
+      $controller.searchQuery = 'searchQuery';
+      $controller.results = [{}];
+      $controller.showResults = true;
+
+      $controller.clearSearch();
+
+      expect($controller.query).toEqual('');
+      expect($controller.searchQuery).toEqual('');
+      expect($controller.results).toEqual([]);
+      expect($controller.showResults).toEqual(false);
+    });
+  });
+
+  describe('.delete', function () {
+
+    it('calls SocketService.emit', function () {
+      var video = {
+        id: {
+          videoId: '1'
+        }
+      };
+
+      spyOn(SocketService, 'emit').and.callThrough();
+
+      $controller.delete(video);
+
+      expect(SocketService.emit).toHaveBeenCalledWith('video deleted', video);
+    });
+  });
+
+  describe('SocketService listeners', function () {
+
+    describe('"socket connected"', function () {
+
+      it('updates controller data with passed data', function () {
+        var data = {
+          messages: [ { message: 'message' }],
+          playlist: [ { videoId: '1' }],
+          users: ['username']
+        };
+
+        socketEvents['socket connected'](data);
+
+        expect($controller.messages).toEqual(data.messages);
+        expect($controller.playlist).toEqual(data.playlist);
+        expect($controller.users).toEqual(data.users);
+      });
+    });
+
+    describe('"login error"', function () {
+
+      it('displays an alert', function () {
+        spyOn(window, 'alert').and.callThrough();
+
+        socketEvents['login error']();
+
+        expect(window.alert).toHaveBeenCalled();
+      });
+    });
+
+    describe('"login success"', function () {
+
+      it('sets showLogin and calls SocketService.emit', function () {
+        spyOn(SocketService, 'emit').and.callThrough();
+
+        expect($controller.showLogin).toBe(true);
+
+        socketEvents['login success']();
+
+        expect($controller.showLogin).toBe(false);
+        expect(SocketService.emit).toHaveBeenCalledWith('current video request');
+      });
+    });
+
+    describe('"user connected"', function () {
+
+      it('adds username to users and usersLower', function () {
+        var username = 'Username';
+
+        socketEvents['user connected'](username);
+
+        expect($controller.users.indexOf(username)).toEqual(0);
+        expect($controller.usersLower.indexOf(username.toLowerCase())).toEqual(0);
+      });
+    });
+
+    describe('"current video response"', function () {
+
+      it('updates currentVideo and calls VideoService.launchPlayer', function () {
+        var video = {
+          startSeconds: 17,
+          video: {
+            videoId: '1'
+          }
+        };
+
+        spyOn(VideoService, 'launchPlayer').and.callThrough();
+
+        socketEvents['current video response'](video);
+
+        expect($controller.currentVideo.startSeconds).toEqual(20);
+        expect(VideoService.launchPlayer).toHaveBeenCalled();
+      });
+    });
+
+    describe('"add video to playlist"', function () {
+
+      it('adds video to playlist and calls VideoService.launchPlayer if only video', function () {
+        var video = {
+          video: {
+            videoId: '1'
+          }
+        };
+
+        spyOn(VideoService, 'launchPlayer').and.callThrough();
+
+        expect($controller.playlist.length).toEqual(0);
+
+        socketEvents['add video to playlist'](video);
+
+        expect($controller.playlist.length).toEqual(1);
+        expect($controller.currentVideo.video).toEqual(video);
+        expect(VideoService.launchPlayer).toHaveBeenCalled();
+      });
+    });
+
+    describe('"delete video from playlist"', function () {
+
+      it('deletes video from playlist', function () {
+        var video = {
+          id: {
+            videoId: '1'
+          }
+        };
+
+        $controller.playlist = [video];
+
+        socketEvents['delete video from playlist'](video);
+
+        expect($controller.playlist.length).toEqual(0);
+      });
+    });
+
+    describe('"user disconnected"', function () {
+
+      it('deletes user from users', function () {
+        var username = 'username';
+
+        $controller.users = [username];
+        $controller.usersLower = [username];
+
+        socketEvents['user disconnected'](username);
+
+        expect($controller.users.length).toEqual(0);
+        expect($controller.usersLower.length).toEqual(0);
+      });
+    });
+
+    describe('"socket refresh request"', function () {
+
+      it('calls /refresh and calls SocketService.emit', function () {
+        var emit = 'emit';
+
+        $httpBackend.when('GET', '/refresh').respond(emit);
+        spyOn(SocketService, 'emit').and.callThrough();
+
+        socketEvents['socket refresh request']();
+
+        $httpBackend.flush();
+
+        expect(SocketService.emit).toHaveBeenCalledWith(emit);
+      });
+    });
+  });
+});
